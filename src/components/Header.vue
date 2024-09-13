7dedcc3aff8dce0751b4c8a16ceda0cdb5e9f312
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-09-13 14:47:36 +0800

    Update Header.vue
    Links!!!

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,11 @@ import Navbar from './Navbar.vue' ;
       return {
         phone_num: '+63 921 240 7455',
         email: 'solcruzmarkanthony@gmail.com',
+        links: [
+          { name: 'facebook', url: 'https://www.facebook.com/CobolRinkashime' },
+          { name: 'github', url: 'https://github.com/Solatido143' },
+          { name: 'linkedin', url: 'https://www.linkedin.com/in/mark-anthony-sol-cruz-2128052ab/' }
+        ]
       }
     }
   }
@@ -15,18 +20,22 @@ import Navbar from './Navbar.vue' ;
 
 <template>
   <header>
-    <div class="container mx-auto bg-dark px-5 hidden lg:block">
+    <div class="container mx-auto bg-dark px-5 hidden md:block mt-2">
       <div class="flex justify-between">
         <div class="flex items-center space-x-4">
           <p><i class="fa fa-phone-alt mr-2"></i>{{ phone_num }}</p>
           <p><i class="fa fa-envelope-open mr-2"></i>{{ email }}</p>
         </div>
         <div class="flex items-center space-x-2">
-          <a class="p-2 bg-white rounded-full" href="#"><i class="fab fa-twitter"></i></a>
-          <a class="p-2 bg-white rounded-full" href="#"><i class="fab fa-facebook-f"></i></a>
-          <a class="p-2 bg-white rounded-full" href="#"><i class="fab fa-linkedin-in"></i></a>
-          <a class="p-2 bg-white rounded-full" href="#"><i class="fab fa-instagram"></i></a>
-          <a class="p-2 bg-white rounded-full" href="#"><i class="fab fa-youtube"></i></a>
+          <a
+          v-for="link in links"
+          :key="link.name"
+          :href="link.url"
+          target="_blank"
+          class="bg-gray-300 hover:bg-gray-100 hover:border-grey-100 hover:border-2 rounded-full"
+          >
+            <i :class="'fab fa-' + link.name"></i>
+          </a>
         </div>
       </div>
     </div>
@@ -36,5 +45,15 @@ import Navbar from './Navbar.vue' ;
 </template>
 
 <style scoped>
-
+  header a {
+    width: 30px;
+    height: 30px;
+    position: relative;
+  }
+  header a i {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
