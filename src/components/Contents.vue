dff02d5b71ca7b613087a36b8b21915a23433973
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-09-16 20:28:37 +0800

    Update Contents.vue

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -50,7 +50,7 @@
       <div class="p-5">
         <div class="flex justify-center items-center">
           <div class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Projects</h5>
+            <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">ProjectsHehe</h5>
             <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">List of all projects that i've done in my lifetime.</p>
 
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -94,11 +94,11 @@
                   <img class="rounded-t-lg" src="" alt="" />
                 </a>
                 <div class="p-5">
-                  <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Lorem ipsum</h5>
+                  <a href="https://solatido-bs-js-calc.vercel.app" target="_blank">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Calculator</h5>
                   </a>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum odor amet, consectetuer adipiscing elit. Malesuada proin placerat class enim viverra vivamus parturient inceptos placerat.</p>
-                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <a href="https://solatido-bs-js-calc.vercel.app" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Go to website
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
