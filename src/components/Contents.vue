b5c9af996c4f5154394cba09bde8bef1bdb6457d
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-09-16 19:22:20 +0800

    Some UI

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -3,16 +3,23 @@
 
 <template>
   <div class="min-h-screen bg-white"><!--  min-h-screen -->
-    <div class="min-h-96 bg-gray-300 p-5">
+    <div class="bg-gray-300">
       <!-- Your content here -->
-      <div class="flex">
-        <div class="w-1/2">
-          Hello
+      <div class="flex flex-col md:flex-row justify-center items-center">
+        <div class="px-5 lg:w-5/12 sm:w-6/12">
+          <div class="my-5">
+            <h1 class="text-4xl font-bold">Mark Anthony Sol Cruz</h1>
+            <p class="mt-4 text-gray-600">
+              Aspiring Software Developer
+            </p>
+          </div>
         </div>
-        <div class="w-1/2">
-          Hi
+
+        <div class="lg:w-5/12 sm:w-6/12">
+          <img class="w-full mx-auto" src="../img/75341273_1431013807047573_5959803062104096768_n.jpg" height="2048" width="1536"/>
         </div>
       </div>
+
     </div>
   </div>
 </template>
