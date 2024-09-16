8f54404bd7b80192220ca0d4014bb14393cdce06
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-09-16 20:24:01 +0800

    Update Contents.vue

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -54,33 +54,59 @@
             <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">List of all projects that i've done in my lifetime.</p>
 
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-              <div>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                  <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="../img/75341273_1431013807047573_5959803062104096768_n.jpg" alt="">
-                  <div class="flex flex-col justify-between p-4 leading-normal">
+
+              <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <img class="rounded-t-lg" src="" alt="" />
+                </a>
+                <div class="p-5">
+                  <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ka-bario</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum odor amet, consectetuer adipiscing elit. Malesuada proin placerat class enim viverra vivamus parturient inceptos placerat.</p>
-                  </div>
-                </a>
+                  </a>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum odor amet, consectetuer adipiscing elit. Malesuada proin placerat class enim viverra vivamus parturient inceptos placerat.</p>
+                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Go to website
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                  </a>
+                </div>
               </div>
-              <div>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                  <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="../img/75341273_1431013807047573_5959803062104096768_n.jpg" alt="">
-                  <div class="flex flex-col justify-between p-4 leading-normal">
+              <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <img class="rounded-t-lg" src="" alt="" />
+                </a>
+                <div class="p-5">
+                  <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nailandia</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum odor amet, consectetuer adipiscing elit. Malesuada proin placerat class enim viverra vivamus parturient inceptos placerat.</p>
-                  </div>
-                </a>
+                  </a>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum odor amet, consectetuer adipiscing elit. Malesuada proin placerat class enim viverra vivamus parturient inceptos placerat.</p>
+                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Go to website
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                  </a>
+                </div>
               </div>
-              <div>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                  <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="../img/75341273_1431013807047573_5959803062104096768_n.jpg" alt="">
-                  <div class="flex flex-col justify-between p-4 leading-normal">
+              <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <img class="rounded-t-lg" src="" alt="" />
+                </a>
+                <div class="p-5">
+                  <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Lorem ipsum</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum odor amet, consectetuer adipiscing elit. Malesuada proin placerat class enim viverra vivamus parturient inceptos placerat.</p>
-                  </div>
-                </a>
+                  </a>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum odor amet, consectetuer adipiscing elit. Malesuada proin placerat class enim viverra vivamus parturient inceptos placerat.</p>
+                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Go to website
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                  </a>
+                </div>
               </div>
+
             </div>
           </div>
         </div>
