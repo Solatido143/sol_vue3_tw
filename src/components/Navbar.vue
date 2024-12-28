d366a509b72e8293f1f3d7085835e44ef5bc53cc
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-12-29 05:51:00 +0800

    hehe

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,95 @@
-<script></script>
+<script setup>
+import {ref} from "vue";
+import {Dialog, DialogPanel} from "@headlessui/vue";
+import {Bars3Icon, XMarkIcon} from "@heroicons/vue/24/outline";
+
+const navigation = [
+	{name: "Home", href: "#", active: true},
+	{name: "About", href: "#about"},
+	{name: "Projects", href: "#projects"},
+	{name: "Skills", href: "#skills"},
+	{name: "Contact", href: "#contact"},
+];
+
+const mobileMenuOpen = ref(false);
+</script>
 
 <template>
-	<nav class="bg-white border-gray-200 dark:bg-gray-900 z-10">
+	<div class="bg-white">
+		<header class="absolute inset-x-0 top-0 z-50">
+			<nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+				<div class="flex lg:flex-1">
+					<a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+						<img src="../img/61226400_1284391731709782_6838077067582504960_n.jpg" class="h-8 rounded-full" alt="Logo" />
+						<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-black"
+							>M.A. Sol Cruz</span
+						>
+					</a>
+				</div>
+				<div class="flex lg:hidden">
+					<button
+						type="button"
+						class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+						@click="mobileMenuOpen = true">
+						<span class="sr-only">Open main menu</span>
+						<Bars3Icon class="size-6" aria-hidden="true" />
+					</button>
+				</div>
+				<div class="hidden lg:flex lg:gap-x-12">
+					<a
+						v-for="item in navigation"
+						:key="item.name"
+						:href="item.href"
+						class="text-sm/6 font-semibold text-gray-900"
+						>{{ item.name }}</a
+					>
+				</div>
+			</nav>
+			<Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+				<div class="fixed inset-0 z-50" />
+				<DialogPanel
+					class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+					<div class="flex items-center justify-between">
+						<a href="#" class="-m-1.5 p-1.5">
+							<span class="sr-only">Your Company</span>
+							<img
+								class="h-8 w-auto"
+								src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+								alt="" />
+						</a>
+						<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+							<span class="sr-only">Close menu</span>
+							<XMarkIcon class="size-6" aria-hidden="true" />
+						</button>
+					</div>
+					<div class="mt-6 flow-root">
+						<div class="-my-6 divide-y divide-gray-500/10">
+							<div class="space-y-2 py-6">
+								<a
+									v-for="item in navigation"
+									:key="item.name"
+									:href="item.href"
+									class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+									>{{ item.name }}</a
+								>
+							</div>
+						</div>
+					</div>
+				</DialogPanel>
+			</Dialog>
+		</header>
+	</div>
+
+	<!-- <nav class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full z-10">
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 			<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-				<img src="../img/61226400_1284391731709782_6838077067582504960_n.jpg" class="h-8 rounded-full" alt="Flowbite Logo" />
-				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-black">M.A. Sol Cruz</span>
+				<img
+					src="../img/61226400_1284391731709782_6838077067582504960_n.jpg"
+					class="h-8 rounded-full"
+					alt="Logo" />
+				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-black"
+					>M.A. Sol Cruz</span
+				>
 			</a>
 			<button
 				data-collapse-toggle="navbar-default"
@@ -65,5 +149,5 @@
 				</ul>
 			</div>
 		</div>
-	</nav>
+	</nav> -->
 </template>
